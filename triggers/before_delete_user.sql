1055dd74450dcca30c5b577f3d149409079e60c5
Kyndrael <jonathansilveira95@hotmail.com>
2025-05-23 09:30:30 -0300

    Create before_delete_user.sql

---
+++ triggers/before_delete_user.sql
@@ -0,0 +1,16 @@
+CREATE TABLE deleted_users_log (
+    user_id INT,
+    user_email VARCHAR(255),
+    deleted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DELIMITER //
+CREATE TRIGGER trg_before_delete_user
+BEFORE DELETE ON users
+FOR EACH ROW
+BEGIN
+    INSERT INTO deleted_users_log (user_id, user_email)
+    VALUES (OLD.user_id, OLD.email);
+END;
+//
+DELIMITER ;
