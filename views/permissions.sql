f390dd0a9ff5b2f7314a7f77c2afb39dce78f293
Kyndrael <jonathansilveira95@hotmail.com>
2025-05-23 09:34:35 -0300

    Update permissions.sql

--- views/permissions.sql
+++ views/permissions.sql
@@ -1,14 +1,14 @@
--- Criar usuários
+-- Criação de usuários
 CREATE USER 'gerente'@'localhost' IDENTIFIED BY 'senha123';
 CREATE USER 'employee'@'localhost' IDENTIFIED BY 'senha123';
 
--- Conceder permissões ao gerente
+-- Permissões para gerente
 GRANT SELECT ON vw_empregados_departamento_localidade TO 'gerente'@'localhost';
 GRANT SELECT ON vw_departamentos_gerentes TO 'gerente'@'localhost';
 GRANT SELECT ON vw_projetos_departamentos_gerentes TO 'gerente'@'localhost';
 GRANT SELECT ON employees TO 'gerente'@'localhost';
 GRANT SELECT ON departments TO 'gerente'@'localhost';
 
--- Conceder permissões ao employee (restrito)
+-- Permissões para funcionário
 GRANT SELECT ON vw_projetos_mais_empregados TO 'employee'@'localhost';
 GRANT SELECT ON vw_empregados_dependentes_gerentes TO 'employee'@'localhost';
